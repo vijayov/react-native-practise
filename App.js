f363646a46a59da8f50d52490cdb49e25b5b2277
vijay <o.vijaykumarn@gmail.com>
2019-05-31 15:07:15 +0530

    redux update

--- App.js
+++ App.js
@@ -3,56 +3,28 @@ import { StyleSheet, Text, View , TextInput , Button} from 'react-native';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
 import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
+import { connect } from 'react-redux';
+import { addPlace,deletePlace,deselectPlace,selectPlace } from './src/store/actions';
 
-export default class App extends React.Component {
-  state = {
-    places : [],
-    selectedPlace : null
-  };
+class App extends React.Component {
+ 
 
-  placeNameChangeHandler = (value) => {
-    this.setState({placeName : value})
-  };
+  
 
   placeAddHandler = (placeName) => {
-   this.setState(prevState => {
-     return {
-       places : prevState.places.concat({
-         key : Math.random(),
-         name : placeName,
-         image : {
-           uri: "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
-         }
-       })
-     }
-   })
+   this.props.onAddPlace(placeName)
   }
 
   placeSelectHandler = (key) => {
-    this.setState(prevState => {
-      return {
-        selectedPlace : prevState.places.find(place => {
-          return place.key === key
-        })
-      }
-    })
+   this.props.onSelectPlace(key)
   }
 
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return place.key !== prevState.selectedPlace.key;
-        }),
-        selectedPlace: null
-      };
-    });
+    this.props.onDeletePlace()
   };
 
   modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    });
+    this.props.onDeselectPlace()
   };
 
 
@@ -60,11 +32,11 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <PlaceDetail selectedPlace={this.state.selectedPlace}
+        <PlaceDetail selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
           onModalClosed={this.modalClosedHandler} />
         <PlaceInput onPlaceAdded={this.placeAddHandler}/>
-        <PlaceList places={this.state.places} onSelectHandler={this.placeSelectHandler}/>
+        <PlaceList places={this.props.places} onSelectHandler={this.placeSelectHandler}/>
       </View>
     )
   }
@@ -80,3 +52,21 @@ const styles = StyleSheet.create({
   },
  
 });
+
+const mapStateToProps = state => {
+  return {
+    places : state.places.places,
+    selectedPlace : state.places.selectedPlace
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace : (name) => dispatch(addPlace(name)),
+    onDeletePlace : () => dispatch(deletePlace()),
+    onSelectPlace : (key) => dispatch(selectPlace(key)),
+    onDeselectPlace : () => dispatch(deselectPlace())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
+import configureStore from './src/store/configureStore';
 
-AppRegistry.registerComponent('firstApp', () => App);
+const store = configureStore();
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent('firstApp', () => RNRedux);
