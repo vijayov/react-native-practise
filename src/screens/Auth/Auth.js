067d1d65962db1ce71ac620a16c989b75ba33928
vijay <o.vijaykumarn@gmail.com>
2019-06-02 11:51:46 +0530

    nav rnn 3 init

---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,14 @@
+import Reace from 'react';
+import {View , Text} from 'react-native';
+
+class AuthScreen extends React.Component {
+    render(){
+        return (
+            <View>
+                <Text>Auth Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default AuthScreen;
